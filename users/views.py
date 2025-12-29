7d34c60d4bd1e667397b753031cdb2d83e555ed4
goenpiloes <goenpiloes@gmail.com>
2025-12-30 00:40:46 +0800

    lesson 24 completed: user edit profile

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import Profile
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 
 
 def loginUser(request):
@@ -54,7 +54,7 @@ def registerUser(request):
             messages.success(request, "User account was created!")
 
             login(request, user)
-            return redirect("profiles")
+            return redirect("edit-account")
         else:
             messages.success(request, "An error was occurred during registration")
 
@@ -95,3 +95,17 @@ def userAccount(request):
         "projects": projects,
     }
     return render(request, "users/account.html", context)
+
+
+@login_required(login_url="login")
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("account")
+    context = {"form": form}
+    return render(request, "users/profile_form.html", context)
