72421b61d1b28ec2533eb72130ef97c4c7eaf061
goenpiloes <goenpiloes@gmail.com>
2026-01-13 15:30:54 +0800

    lesson 30 completed: add feature "reviews"

--- projects/models.py
+++ projects/models.py
@@ -22,7 +22,23 @@ class Project(models.Model):
         return str(self.title)
 
     class Meta:
-        ordering = ["-created"]
+        ordering = ["-vote_ratio", "-vote_total", "title"]
+
+    @property
+    def reviewers(self):
+        queryset = self.review_set.all().values_list("owner__id", flat=True)
+        return queryset
+
+    @property
+    def getVoteCount(self):
+        reviews = self.review_set.all()
+        upVotes = reviews.filter(value="up").count()
+        totalVotes = reviews.count()
+
+        ratio = (upVotes / totalVotes) * 100
+
+        self.vote_total = totalVotes
+        self.vote_ratio = ratio
 
 
 class Review(models.Model):
@@ -30,7 +46,7 @@ class Review(models.Model):
         ("up", "Up Vote"),
         ("down", "Down Vote"),
     )
-    # owner =
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=200, choices=VOTE_TYPE)
@@ -38,6 +54,9 @@ class Review(models.Model):
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, primary_key=True, editable=False
     )
+
+    class Meta:
+        unique_together = [["owner", "project"]]
 
     def __str__(self):
         return str(self.value)
